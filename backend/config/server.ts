00d7fd30d35c8dc65e4285e535e35a60bf21137f
Fredrik <fredrik.fredriksson@coin360.com>
2025-06-11 19:52:44 +0200

    New updates

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,17 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  url: env("URL", "https://your-app-url.com"),
+  proxy: env.bool("IS_PROXIED", true),
+  cron: {
+    enabled: env.bool("CRON_ENABLED", false),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
   },
 });
